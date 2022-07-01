7ad26663f00c348b1508e7f712507c263b788964
堀裕成 <horyusei@Holy-Air.local>
2022-07-01 11:40:39 +0900

    最初のGit

---
+++ foot.php
@@ -0,0 +1,14 @@
+<footer class="mt-5">
+
+<div class="copyright">
+    2022(C) Hori Allright Reserved. 
+</div>
+
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once "head.php";
+?>
+
+<title>ウェブサイト</title>
+</head>
+
+<body>
+    <?php
+    require_once "nav.php";
+    ?>
+
+    <img src="image/fv.jpeg" alt="" class="w-100 mb-5">
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="image/fv.jpeg" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <img src="image/fv.jpeg" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <img src="image/fv.jpeg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+
+    <?php
+    require_once "foot.php";
+    ?>
